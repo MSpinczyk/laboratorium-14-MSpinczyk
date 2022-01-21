6155cf0b3c399054a7e154bf3e8cb9d34cfa13d4
Michal_Spinczyk <michal.spinczyk@gmail.com>
2022-01-21 16:12:32 +0100

    First commit

---
+++ myapp/app.py
@@ -0,0 +1,37 @@
+from textblob import TextBlob
+
+def hello(name):
+    output = f'Hello {name}'
+    return output
+
+def extract_sentiment(text):
+    text = TextBlob(text)
+
+    return text.sentiment.polarity
+
+def text_contain_word(word: str, text: str):
+    
+    return word in text
+
+def bubble_sort(list_1: list):
+    if not isinstance(list_1,list):
+        return None
+    n = len(list_1)
+    while n > 0:
+        for i in range(0,n-1):
+            if list_1[i] > list_1[i+1]:
+                el = list_1[i]
+                list_1[i] = list_1[i+1]
+                list_1[i+1] = el
+        n = n-1
+    return list_1
+
+def matrix_multiplication(m1,m2):
+    result = [[0,0,0],[0,0,0],[0,0,0]]
+    for i in range(len(m1)):
+        for j in range(len(m2[0])):
+            for k in range(len(m2)):
+                result[i][j] += m1[i][k] * m2[k][j]
+    return result
+
+print(matrix_multiplication([[1,2,3],[1,2,3],[1,2,3]],[[1,1,1],[1,1,1],[1,1,1]]))
---
+++ myapp/test/test_app.py
@@ -0,0 +1,54 @@
+from app import hello, extract_sentiment, text_contain_word, bubble_sort, matrix_multiplication
+import pytest
+
+def test_hello():
+    got = hello("Aleksandra")
+    want = "Hello Aleksandra"
+
+    assert got == want
+
+
+testdata = ["I think today will be a great day","I do not think this will turn out well"]
+
+@pytest.mark.parametrize('sample', testdata)
+def test_extract_sentiment(sample):
+
+    sentiment = extract_sentiment(sample)
+
+    assert sentiment > 0
+
+
+testdata = [
+    ('There is a duck in this text', 'duck', True),
+    ('There is nothing here', 'duck', False)
+    ]
+
+@pytest.mark.parametrize('sample, word, expected_output', testdata)
+def test_text_contain_word(sample, word, expected_output):
+
+    assert text_contain_word(word, sample) == expected_output
+
+
+testdata = [
+    ([4,3,7,9,1,4,6,8,1], [1,1,3,4,4,6,7,8,9]),
+    ([9,8,7,6,5,4,3,2,1], [1,2,3,4,5,6,7,8,9]),
+    ([2,3,2,3,2,3,2,3,2], [2,2,2,2,2,3,3,3,3]),
+    ([],[]),
+    ([11,12,13,14,8,7,5], [5,7,8,11,12,13,14]),
+    ('string', None)
+    ]
+
+@pytest.mark.parametrize('sample, expected_output', testdata)
+def test_bubble_sort(sample, expected_output):
+
+    assert bubble_sort(sample) == expected_output
+
+
+testdata = [
+    ([[1,2,3]],[[1],[1],[1]], [[6]])
+    ]
+
+@pytest.mark.parametrize('m1, m2, expected_output', testdata)
+def test_matrix_multiplication(m1, m2, expected_output):
+
+    assert matrix_multiplication(m1, m2) == expected_output
